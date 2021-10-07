39e9814d261edcf08ff25606cfc90717245197cf
Rodrigo Tomaz <rodrigo_souza2013@yahoo.com.br>
2021-10-06 22:32:07 -0300

    OrderStatus enum

---
+++ src/main/java/com/glue/curso/entities/enuns/OrderStatus.java
@@ -0,0 +1,30 @@
+package com.glue.curso.entities.enuns;
+
+public enum OrderStatus {
+
+	WAITING_PAYMENT(1),
+	PAID(2),
+	SHIPED(3),
+	DELIVERY(4),
+	CANCELED(5);
+	
+	private int code;
+	
+	private OrderStatus(int code) {
+		this.code = code;
+	}
+	
+	public int getCode() {
+		return code;
+	}
+	
+	public static OrderStatus valueOf(int code) {
+		for(OrderStatus value: OrderStatus.values()) {
+			if(value.getCode() == code) {
+				return value;
+			}
+		}
+		
+		throw new IllegalArgumentException("Invalid OrderStatus code");
+	}
+}
