27cad71fab6944d80ad147c2c57986a99e703540
Rodrigo Tomaz <rodrigo_souza2013@yahoo.com.br>
2021-10-10 13:27:35 -0300

    Exception handling - findById

--- src/main/java/com/glue/curso/services/OrderService.java
+++ src/main/java/com/glue/curso/services/OrderService.java
@@ -7,9 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.glue.curso.entities.Order;
-import com.glue.curso.entities.User;
 import com.glue.curso.repositories.OrderRepository;
-import com.glue.curso.repositories.UserRepository;
 
 @Service
 public class OrderService {
